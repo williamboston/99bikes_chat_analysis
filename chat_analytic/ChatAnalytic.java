18a68f101520b544f866676a6ace80d30505442c
William <williamboston1@gmail.com>
2020-02-14 22:27:06 +1000

    CSV reading now works

--- chat_analytic/ChatAnalytic.java
+++ chat_analytic/ChatAnalytic.java
@@ -1,44 +1,44 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
 
 public class ChatAnalytic {
     public static void main(String args[]) {
-        // open csv
-        // scan for certain tags
-        // append to list or something
-        // append those numbers to a local database if you're into that OR another CSV (this might make it easier to display rather than having to then read the DB and display in the terminal or some shit)
-        System.out.println("code compiling and running");
 
-        String[] tags = {"frequent_visitor", "mtb", "road", "bmx", "sale", "commuter", "e-bike", "scooter", "2hb", };
-        // Tag Array
-        // String[] tagStrings;
+        // array of known tags
+        String[] tags = {"frequent_visitor", "mtb", "road", "bmx", "sale", "commuter", "e-bike", "scooter", "2hb"};
+        // list of found tags
+        List<String> finalTags = new ArrayList<String>();
 
         //first args is file name (locally)
         String csv = args[0];
         BufferedReader file = null;
         String line = "";
-        String csvSplitBy = ",";
+        String line2 = "";
+        String delim = ",";
 
+        // block to find all valid tags from given csv file
         try {
             file = new BufferedReader(new FileReader(csv));
-
+ 
             //need a way to distinguish between commas in tags col and csv commas
             while ((line = file.readLine()) != null) {
-                String[] tag = line.split(csvSplitBy);
-                //clean the strings of quote marks
+                String[] tag = line.split(delim);
+                //clean the strings of quote marks 
                 for (String retVal : tag) {
                     if (retVal.startsWith("\"") || retVal.endsWith("\"")) {
                         retVal = retVal.replace("\"", "");
-                        // System.out.println("string cleaned...");
+                        finalTags.add(retVal);
                     }
                 }
                 // check for match
                 for (String ret : tag) {
                     for (String match : tags) {
                         if (ret.equals(match)) {
-                            System.out.println(ret);
+                            finalTags.add(ret);
                         }
                     }
                 }
@@ -57,5 +57,37 @@ public class ChatAnalytic {
                 }
             }
         }
+
+        //for testing
+        for (String tag : finalTags) {
+            System.out.println(tag);
+        }
+
+        // open existing csv
+        try {
+            FileWriter output = new FileWriter("chat_counter.csv", true);
+
+            for (String tag : finalTags) {
+                output.append(tag);
+                output.append("\n");
+            }
+
+            output.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (file != null) {
+                try {
+                    file.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        // 
+
+
     }
 }
